e2e48ec0c9dc05b9df7c1e90663cf99996f61e70
Anna <anna@as-MacBook-Pro.local>
2022-02-01 18:46:50 +0200

    add fixed

--- src/Parse.js
+++ src/Parse.js
@@ -1,32 +1,44 @@
 import React from "react";
+import { useState } from "react";
 
 
 const url = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTjcwOmp3Wq2YqbMc2mI44uDnUhdun8weGiEdQlQi9EvuUnrQKloLTMoXJFeLwDuc6RYAgHq4RlzoDs/pub?output=tsv';
 
-const Parse = text => {
-  const arr = text.toString().split('\r\n').map(str => str.split('\t'));
-  const names = arr.shift();
-  const logs = arr.map(log => log.reduce((acc, el, i) => {
-    acc[names[i]] = el;
-    return acc;
-  }, {}));
+const Parse = () => {
+
+  const [logsArray, setLogsArray] = useState([])
+  const [namesArray, setNamesArray] = useState([])
+
+  console.log(namesArray, Date.now())
+
+  if(logsArray.length == 0){
+    fetch(url).then(r => r.text()).then((text)=> {
+      const arr = text.toString().split('\r\n').map(str => str.split('\t'));
+      const names = arr.shift();
+      const logs = arr.map(log => log.reduce((acc, el, i) => {
+        acc[names[i]] = el;
+        return acc;
+      }, {}));
+      setLogsArray(logs)
+      setNamesArray(names)
+    });
+  }
+  
   
 
   return(
     <table>
       <thead>
         <tr>
-          <th>{names[0]}</th>
-          <th>{names[1]}</th>
-          <th>{names[2]}</th>
-          <th>{names[3]}</th>
-          <th>{names[4]}</th>
-          <th>{names[5]}</th>
-          <th>{names[6]}</th>
+        {namesArray.map((name, id) => {
+          return (
+            <th key={name + id}>{name}</th>
+          )
+        })}
         </tr>
       </thead>
       <tbody>
-        {logs.map(log => {
+        {logsArray.map(log => {
               return (
                 <tr key={log.id}>
                     <td>{log.id}</td>
@@ -39,15 +51,11 @@ const Parse = text => {
                 </tr>
               )
         })}
-        {console.log(logs)}
-        {console.log(names)}
       </tbody>
     </table>
   
   )
 }
-
-fetch(url).then(r => r.text()).then(Parse);
 
 
 export default Parse;
